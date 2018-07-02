17a7c5861f6884732ef4777104eeb89e7892fad4
Christian Gullin <krich_80@hotmail.com>
2018-07-03 00:04:47 +0200

    Hantering av manuellt ställd belysning

--- scenes/turn-light-on-at-sunset.lua
+++ scenes/turn-light-on-at-sunset.lua
@@ -5,16 +5,21 @@
 %% events
 %% globals
 --]]
+
 -- Tänder när:
 --  * alla dagar i veckan,
+--  * 90 minunter före solnedgång och
+--  * om ingen scen är körd manuellt sedan morgonen kl. 03:00
+--    styrs av logiska variabeln isLightSceneManSet som sätts i manuell scener
 
 local currentDate = os.date("*t")
 if
-    ((currentDate.wday == 1 or currentDate.wday == 2 or currentDate.wday == 3 or currentDate.wday == 4 or
+    (((currentDate.wday == 1 or currentDate.wday == 2 or currentDate.wday == 3 or currentDate.wday == 4 or
         currentDate.wday == 5 or
         currentDate.wday == 6 or
         currentDate.wday == 7) and
-        os.date("%H:%M", os.time() + 90 * 60) == fibaro:getValue(1, "sunsetHour"))
+        os.date("%H:%M", os.time() + 90 * 60) == fibaro:getValue(1, "sunsetHour")) and
+        fibaro:getGlobalValue("isLightSceneManSet") == "falskt")
  then
     fibaro:startScene(22)
 end
