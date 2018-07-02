3b9804552dceb113ea296298d111dbc7a26564b0
Christian Gullin <krich_80@hotmail.com>
2018-07-02 23:45:57 +0200

    Initial commit. Hantera lamptändning vid solnedgång.

---
+++ scenes/turn-light-on-at-sunset.lua
@@ -0,0 +1,22 @@
+--[[
+%% autostart
+%% properties
+%% weather
+%% events
+%% globals
+--]]
+-- Tänder när:
+--  * alla dagar i veckan,
+
+local currentDate = os.date("*t")
+if
+    ((currentDate.wday == 1 or currentDate.wday == 2 or currentDate.wday == 3 or currentDate.wday == 4 or
+        currentDate.wday == 5 or
+        currentDate.wday == 6 or
+        currentDate.wday == 7) and
+        os.date("%H:%M", os.time() + 90 * 60) == fibaro:getValue(1, "sunsetHour"))
+ then
+    fibaro:startScene(22)
+end
+
+setTimeout(tempFunc, 60 * 1000)
